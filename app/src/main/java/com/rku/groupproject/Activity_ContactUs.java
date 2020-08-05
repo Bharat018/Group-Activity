82a3a4ea90bf375fdafd41b7e6186accf9dfa8ac
Bharat018 <bpedhadiya192@rku.ac.in>
2020-08-05 13:58:23 +0530

    "@GroupProject/create"

---
+++ app/src/main/java/com/rku/groupproject/Activity_ContactUs.java
@@ -0,0 +1,14 @@
+package com.rku.groupproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_ContactUs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__contactus);
+    }
+}
